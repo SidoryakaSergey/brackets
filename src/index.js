1ff9047de3d2aa049f36a3012fe68bfcb1631d96
Thanatus <s.sidoryaka@gmail.com>
2023-01-22 19:59:20 +0200

    the solution of the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2) {
+    return false;
+  }
+  const openBrackets = [];
+  const closeBrackets = [];
+  const otherBrackets = [];
+  const stake = [];
+  for (let el of bracketsConfig) {
+    if (el[0] === el[1]) {
+      otherBrackets.push(el[0]);
+    } else {
+      openBrackets.push(el[0]);
+      closeBrackets.push(el[1]);
+    }
+  }
+
+  function getBracket(bracket) {
+    return closeBrackets[openBrackets.indexOf(bracket)];
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    if (otherBrackets.includes(str[i])) {
+      if (stake.includes(str[i])) {
+        if (stake.pop() !== str[i]) {
+          return false;
+        } else {
+          continue;
+        }
+      } else {
+        stake.push(str[i]);
+        continue;
+      }
+    }
+    if (openBrackets.includes(str[i])) {
+      stake.push(str[i]);
+    } else {
+      if (getBracket(stake.pop()) === str[i]) {
+        continue;
+      } else {
+        return false;
+      }
+    }
+  }
+  return true;
+};
